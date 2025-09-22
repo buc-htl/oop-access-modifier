2c7494d872b17454ad433fcd8effe737dc3fa82f
Reinhold Buchinger <55023682+buc-htl@users.noreply.github.com>
2025-09-22 10:20:46 +0200

    Improve comment formatting in Bottle.java

--- src/Bottle.java
+++ src/Bottle.java
@@ -1,7 +1,8 @@
 /**
  In der Objektorientierung kennt man so genannte Zugriffsmodifizierer (engl. access modifier), die die Rechte anderer Objekte einschränken.
 
- Die (Daten)-Kapselung ist eine Strategie in der objektorientierten Programmierung um ungewollte Änderungen an Objekten von „außen“ zu verhindern und die Abhängigkeiten zwischen Klassen zu reduzieren. So sind einfacher Änderungen an einer Klasse möglich ohne Änderungen im gesamten Code vornehmen zu müssen.
+ Die (Daten)-Kapselung ist eine Strategie in der objektorientierten Programmierung um ungewollte Änderungen an Objekten von „außen“ zu verhindern 
+ und die Abhängigkeiten zwischen Klassen zu reduzieren. So sind einfacher Änderungen an einer Klasse möglich ohne Änderungen im gesamten Code vornehmen zu müssen.
 
  Zugriffsmodifizierer wie z.B. "public" können vor Klassen, Attributen oder Methoden stehen.
 
@@ -13,15 +14,24 @@
 
  Es werden hier zwar alle erklärt, im Moment sind aber nur public und private für uns relevant.
 
- "private" ist der restriktivste Zugriffsmodifizierer. Er verbietet jeglichen Zugriff von außerhalb der Klasse. Auf eine private Variable kann nur die Klasse selbst zugreifen, ebenso auf einen privaten Konstruktor, eine private Methode. Klassenvariablen werden üblicherweise als private deklariert. Das Verändern der Variablen wird über passende Methoden, meist Getter und Setter ermöglicht. Damit erreichen wir eine (Daten)-Kapselung.
+ "private" ist der restriktivste Zugriffsmodifizierer. Er verbietet jeglichen Zugriff von außerhalb der Klasse. 
+ Auf eine private Variable kann nur die Klasse selbst zugreifen, ebenso auf einen privaten Konstruktor, eine private Methode. 
+ Klassenvariablen werden üblicherweise als private deklariert. Das Verändern der Variablen wird über passende Methoden, meist Getter und Setter ermöglicht.
+ Damit erreichen wir eine (Daten)-Kapselung.
 
- Als "default" bezeichnet man die Zugreifbarkeit für den Fall, dass kein Zugriffsmodifizierer angegeben wurde. Auf einen default Member können nur Klassen zugreifen, die sich im selben Paket wie die Klasse des Members befinden. Dieser Zugriffmodifizierer wird insbesondere bei der Entwicklung von API eingesetzt.
+ Als "default" bezeichnet man die Zugreifbarkeit für den Fall, dass kein Zugriffsmodifizierer angegeben wurde. 
+ Auf einen default Member können nur Klassen zugreifen, die sich im selben Paket wie die Klasse des Members befinden. 
+ Dieser Zugriffmodifizierer wird insbesondere bei der Entwicklung von API eingesetzt.
 
- Mit dem Zugriffsmodifizierer "protected" ist der Zugriff nicht nur Klassen aus dem selben Package (wie "default"), sondern auch Subklassen der Klasse erlaubt. Dies gilt auch, wenn die betreffenden Subklassen aus einem anderen Package sind als die Klasse des betreffenden Members. Der Zugriffsmodifizierer "protected" wird verwendet, wenn es nur für Subklassen Sinn ergibt, den betreffenden Member zu verwenden.
+ Mit dem Zugriffsmodifizierer "protected" ist der Zugriff nicht nur Klassen aus dem selben Package (wie "default"), sondern auch Subklassen der Klasse erlaubt. 
+ Dies gilt auch, wenn die betreffenden Subklassen aus einem anderen Package sind als die Klasse des betreffenden Members. 
+ Der Zugriffsmodifizierer "protected" wird verwendet, wenn es nur für Subklassen Sinn ergibt, den betreffenden Member zu verwenden.
 
- Der Zugriffsmodifizierer "public" gestattet sämtlichen Klassen Zugriff auf den betreffenden Member. Er ist der freizügigste Zugriffsmodifizierer. Die Zugreifbarkeit public findet man hauptsächlich bei Methoden, die von anderen Klassen verwendet werden sollen
+ Der Zugriffsmodifizierer "public" gestattet sämtlichen Klassen Zugriff auf den betreffenden Member. Er ist der freizügigste Zugriffsmodifizierer. 
+ Die Zugreifbarkeit public findet man hauptsächlich bei Methoden, die von anderen Klassen verwendet werden sollen.
 
  **/
+
 public class Bottle {
 
     /*Generell möchte man nicht, dass von außen direkt auf Attribute zugegriffen werden kann. Damit würde man die Kontrolle verlieren und nicht mehr überprüfen können, welche Werte gesetzt werden. Außerdem könnte man z.B. nicht einfach den Datentyp oder den Namen ändern, ohne dass überall im Code, wo auf diese Attribute zugegriffen wird, Änderungen vorgenommen werden müssen.
